629470b0bc5c6b69802d1b5d5bc2178dbf065a01
Mirkan <mirkancaliskan@gmail.com>
2021-10-04 12:34:02 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Mirkan Çalışkan on 4.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
